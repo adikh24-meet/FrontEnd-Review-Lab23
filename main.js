0b0b98f674bcce12286bd51a9c276cf866fa3ff6
adikh24-meet <139681014+adikh24-meet@users.noreply.github.com>
2023-07-17 18:17:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function changeBackgroundColor(color){
+	if (document.body.style.backgroundColor == color)
+	document.body.style.backgroundColor = #0093;
+	else
 	document.body.style.backgroundColor = color;
+	
 }
